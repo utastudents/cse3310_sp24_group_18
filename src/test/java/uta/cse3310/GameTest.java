0f4d955742c9519da0056d2a154036fd4db569d6
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-04-24 11:33:40 -0500

    Updated GameTest.java

--- src/test/java/uta/cse3310/GameTest.java
+++ src/test/java/uta/cse3310/GameTest.java
@@ -25,16 +25,36 @@ public class GameTest
     }
 
     public void testCase(){
-    Game g = new Game("TestLobby", "TestRoom");
-    Player p1 = new Player("josh", null);
-    Player p2 = new Player("adam", null);
 
+    Game g = new Game("TestLobby", "TestRoom"); // New test Room
+    Player p1 = new Player("josh", null); // new player named josh
+    Player p2 = new Player("adam", null); // new player named adam
+
+        //adding players to game
     g.addPlayer(p1);
     g.addPlayer(p1);
     g.setPlayer1(p1);
     g.setPlayer2(p2);
 
+        //checkking to see if game is full, adn ready to start
     assertTrue(g.isReadyToStart());
     assertEquals(g.getCurrentNumberOfPlayers(),"2");
+
+    //check to see if words can be correctlr chosen by players
+    assertTrue(g.checkWord(p1.getUsername(), "Pagani")); // these next few words need to be changed if TXT file changes
+    g.checkWord(p1.getUsername(), "Pagani"); // josh finds word pagani
+    g.checkWord(p2.getUsername(), "Maserati"); // adam finds word Maserati
+    g.checkWord(p2.getUsername(), "Aspark"); // adam finds word Aspark
+
+    // retrieve player scores
+    String string = String.valueOf(g.getPlayerScores());
+    assertEquals(string, "{adam=2, josh=1}"); // check player scores.
+
+    // reset game and kick players out
+    g.reset();
+    
+    //make sure there are no players left in the lobby
+    assertEquals(g.getCurrentNumberOfPlayers(),"0");
+
     }
 }
