ef4a34d4e35d54c4ce16473c9d8b7b50d75d1946
Dan45RS <67072831+Dan45RS@users.noreply.github.com>
2024-04-30 21:42:03 -0500

    Component Testing before use of system

--- src/main/resources/archetype-resources/src/test/java/AppTest.java
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -1,38 +1,41 @@
-package $org.example;
+package uta.cse3310;
+
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+
+public class AppTest extends TestCase {
+
+    public AppTest(String testName) {
+        super(testName);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    
+    public void testApp() {
+    //     // Test case 1: Ensure playerMap is empty initially
+    //     App app = new App(8080);
+    //     assertTrue(app.getPlayerMap().isEmpty());
+
+    //     // Test case 2: Ensure gameMap is empty initially
+    //     assertTrue(app.getGameMap().isEmpty());
+
+        App A = new App(8070);
+
+        Player P = new Player("abcd", null);
+        P.setInGameScore(0);
+        P.incrementScore();
+        assertTrue(P.getInGameScore() != 0);
+
+        Chat C = new Chat();
+        String message = "Yo";
+        C.addMessage(message);
+        assertTrue(C.getMessages().contains(message));
+
     }
-}
+}   
